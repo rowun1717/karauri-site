7f6c117a4e0495772be1d0c61e074e24853ed37e
rowun1717 <karauri1717@gmail.com>
2026-07-03 23:42:56 +0900

    Add files via upload

---
+++ straykids/songs.js
@@ -0,0 +1,415 @@
+const songs = [
+  {
+    title: "MANIAC",
+    album: "ODDINARY",
+    type: "Original Song",
+    date: "2026.06.12",
+    url: "song/MANIAC.html",
+    image: "images/MANIAC.jpg",
+
+    members: [
+      "Bang Chan",
+      "バンチャン",
+      "Changbin",
+      "チャンビン",
+      "HAN",
+      "ハン",
+      "Felix",
+      "フィリックス",
+      "Hyunjin",
+      "ヒョンジン",
+      "Lee Know",
+      "リノ",
+      "Seungmin",
+      "スンミン",
+      "I.N",
+      "アイエン"
+    ],
+
+    lyrics: `
+      Let's go
+
+
+じょんさんいん ちょk たどぅr ひmじょmっぺ
+정상인 척 다들 힘 좀 빼
+まともなフリしてないで力を抜いたら？
+
+
+じっこ いんぬん みそどぅるん っせへ
+짓고 있는 미소들은 쎄해
+浮かべてる微笑みは冷たくてぎこちない
+
+
+
+らき ぷrりみょん だ っとっかっち
+Lock이 풀리면 다 똑같지
+Lockが解けたらみんな同じ
+
+
+ぬぬん なr もっそぎょ ho
+눈은 날 못 속여 ho
+瞳は僕を騙せないよ ho
+
+
+
+ぼんちぇぬん ぷrりょんね
+본체는 풀렸네
+本体は自由になった
+
+
+じょんしぬr がんしに じゃpっち
+정신을 간신히 잡지
+辛うじて正気を保ってる
+
+
+ぬなんぼん っかmっぱぎご back
+눈 한번 깜빡이고 back
+一度瞬きしてback
+
+
+だし せさんい じょんはん じょんさんいん
+다시 세상이 정한 정상인 
+また世の中が決めた普通の人の
+
+
+こすぷれ じゅんび ppow
+코스프레 준비 ppow
+コスプレの準備 ppow
+ 
+
+
+Mash up, mind blown 
+
+
+じょんしぬん back up
+정신은 back up
+正気は back up
+
+
+Prototype 
+
+
+ね そぐん のんじぇな freaky monster
+내 속은 언제나 freaky monster
+僕の中身はいつも freaky monster
+
+
+ゆへん がとぅん ちんじょらむん ちょり じな rotten
+유행 같은 친절함은 철이 지나 rotten
+流行りみたいな親切さは旬が過ぎて rotten
+
+
+ね とんすえ だ しうぉなげ よけど だ もっくm
+내 통수에 다 시원하게 욕해도 다 먹금
+僕の後頭部に思い切り罵ったって興味ないよ
+
+
+
+Poppin'
+
+
+すんじなげまん ぼだが くげ だちm
+순진하게만 보다가 크게 다침
+無邪気なだけだと思ってたら大怪我するよ
+
+
+ほうぃが げそkっとぇみょん ぐぉrりいん じゅらね toxic
+호의가 계속되면 권리인 줄 아네 toxic
+善意が繰り返されれば権利だと思ってるだろ toxic
+
+
+いろに どrじ warning
+이러니 돌지 warning
+こんなだからおかしくなっていくんだ warning
+
+
+
+MANIAC
+
+
+なさ っぱじん ごっちょろm みちょ MANIAC
+나사 빠진 것처럼 미쳐 MANIAC
+ネジが飛んだみたいに狂え MANIAC
+
+
+ぴんぴん どらぼりげっち
+핑핑 돌아버리겠지
+くらくら おかしくなりそうだろ
+
+
+MANIAC Frankensteinちょろm ごろ
+MANIAC Frankenstein처럼 걸어
+MANIAC Frankensteinみたいに歩くんだ
+
+
+MANIAC MANIAC Haha
+
+
+
+MANIAC (Oh)
+
+
+なさ っぱじん ごっちょろm うそ MANIAC
+나사 빠진 것처럼 웃어 MANIAC 
+ネジが飛んだみたいに笑え MANIAC
+
+
+(You can't stop the smoke)
+
+
+ぴんぴん どらぼりげっち
+핑핑 돌아버리겠지 
+くらくら おかしくなりそうだろ
+
+
+(Thick as fog)
+
+
+MANIAC びじょんさんとぅそんい じpったん
+MANIAC 비정상투성이 집단 
+MANIAC 異常者だらけの集団
+
+
+(We're MANIACS)
+
+
+MANIAC MANIAC
+
+
+
+だ とじん ににょん しrっぱpちょろm
+다 터진 인형 실밥처럼
+ほつれたぬいぐるみの縫い目のように
+
+
+ぎょrぐk ぼんせぎ どぅろなじ
+결국 본색이 드러나지
+いずれ本性はバレるんだよ
+
+
+ぴょなじ あぬんに life
+편하지 않은 이 life
+楽じゃないこのlife
+
+
+It ain't “live” it's “holding on” yeah
+
+
+
+じょんさんいん ちょk だどぅr ちょkっちょmっぺ
+정상인 척 다들 척 좀 빼
+まともなフリはもうやめたら
+
+
+じっこいんぬん みそ no fresh へ
+짓고 있는 미소 no fresh 해
+浮かべてる微笑み no freshだ
+
+
+Locket ぷrみょん だどぅr っとっかっち
+Locket 풀면 다들 똑같지
+Locketを開けたらみんな同じ
+
+
+ぬぬん なr もっそぎょ ho
+눈은 날 못 속여 ho
+瞳は僕を騙せないよ ho
+
+
+
+ねが ごんぬん に ごりぬん だ じるぇばっ
+내가 걷는 이 거리는 다 지뢰밭
+僕が歩くこの道は全て地雷原
+
+
+だ おんじぇ とじrじ もるぬん dormant volcano
+다 언제 터질지 모르는 dormant volcano
+みんないつ爆発するかわからない dormant volcano
+
+
+やmじょねっとん ばらmど おんじぇ ばっくぃrじ もrら
+얌전했던 바람도 언제 바뀔지 몰라
+静かだった風もいつ変わるかわからない
+
+
+だどぅr すmぎん ちぇ さらが
+다들 숨긴 채 살아가 
+みんな隠したまま生きていくんだ
+
+
+like a sealed tornado
+
+
+
+Poppin'
+
+
+すんじなげまん ぼだが くげ だちm
+순진하게만 보다가 크게 다침
+無邪気なだけだと思ってたら大怪我するよ
+
+
+ほうぃが げそkっとぇみょん ぐぉrりいん じゅらね toxic
+호의가 계속되면 권리인 줄 아네 toxic
+善意が繰り返されたら権利だと思ってるだろ toxic
+
+
+いろに どrじ warning
+이러니 돌지 warning
+こんなだからおかしくなっていくんだ warning
+
+
+
+MANIAC
+
+
+なさ っぱじん ごっちょろm みちょ MANIAC
+나사 빠진 것처럼 미쳐 MANIAC
+ネジが飛んだみたいに狂え MANIAC
+
+
+ぴんぴん どらぼりげっち
+핑핑 돌아버리겠지
+くらくら おかしくなりそうだろ
+
+
+MANIAC Frankensteinちょろm ごろ
+MANIAC Frankenstein처럼 걸어
+MANIAC Frankensteinみたいに歩くんだ
+
+
+MANIAC MANIAC Haha
+
+
+
+MANIAC (Oh)
+
+
+なさ っぱじん ごっちょろm うそ MANIAC
+나사 빠진 것처럼 웃어 MANIAC 
+ネジが飛んだみたいに笑え MANIAC
+
+
+(You can't stop the smoke)
+
+
+ぴんぴん どらぼりげっち
+핑핑 돌아버리겠지 
+くらくら おかしくなりそうだろ
+
+
+(Thick as fog)
+
+
+MANIAC びじょんさんとぅそんい じpったん
+MANIAC 비정상투성이 집단 
+MANIAC 異常者だらけの集団
+
+
+(We're MANIACS)
+
+
+MANIAC MANIAC
+
+
+
+かどぅけ どぅ ぬぬん lunatic
+가득해 두 눈은 lunatic
+満ちてる 両目にはlunatic
+
+
+もどぅん がmがぎ なr そ いっち
+모든 감각이 날 서 있지
+全神経が過敏になってるよ
+
+
+いぇっぷげ ぽじゃんはん でろ めぼん がどぅぉ のうに
+예쁘게 포장한 대로 매번 가둬 놓으니
+綺麗に包んだままずっと封じ込めておくと
+
+
+ふrろがだ ぼみょん ぎょrぐkっとぅろなげっち
+흘러가다 보면 결국 드러나겠지
+時間が流れたらいずれバレるだろうね
+
+
+すmぎょじん ねみょね ぐ もすび yeah
+숨겨진 내면의 그 모습이 yeah
+隠された内側のその姿が yeah
+
+
+
+MANIAC
+
+
+MANIAC (MANIAC)
+
+
+MANIAC
+
+
+MANIAC MANIAC
+
+
+You cannot stop with this feeling
+
+
+
+MANIAC (Oh)
+
+
+なさ っぱじん ごっちょろm うそ MANIAC
+나사 빠진 것처럼 웃어 MANIAC 
+ネジが飛んだみたいに笑え MANIAC
+
+
+(You can't stop the smoke)
+
+
+ぴんぴん どらぼりげっち 
+핑핑 돌아버리겠지 
+くらくら おかしくなりそうだろ
+
+
+(Thick as fog)
+
+
+MANIAC びじょんさんとぅそんい じpったん
+MANIAC 비정상투성이 집단 
+MANIAC 異常者だらけの集団
+
+
+(We're MANIACS)
+
+
+MANIAC MANIAC
+    `
+  },
+
+  {
+    title: "CASE 143",
+    album: "MAXIDENT",
+    type: "Original Song",
+    date: "2026.06.12",
+    url: "song/CASE143.html",
+    image: "images/CASE143.jpg",
+
+    members: [
+      "Bang Chan",
+      "Lee Know",
+      "Changbin",
+      "Hyunjin",
+      "HAN",
+      "Felix",
+      "Seungmin",
+      "I.N"
+    ],
+
+    lyrics: `
+      CASE 143
+      love
+      heart
+      why do I keep getting attracted
+    `
+  }
+];
