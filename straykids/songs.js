b020bf01dbb6c0edefdb19af34bfd2d87315cada
rowun1717 <karauri1717@gmail.com>
2026-07-26 22:37:36 +0900

    Add files via upload

---
+++ straykids/songs.js
@@ -0,0 +1,244 @@
+const songs = [
+  {
+    title: "MANIAC",
+    album: "ODDINARY",
+    type: "Original Song",
+    date: "2026.06.12",
+    url: "song/MANIAC.html",
+    image: "images/MANIAC.jpg",
+
+    members: [
+      "Bang Chan",
+      "バンチャン",
+      "Changbin",
+      "チャンビン",
+      "HAN",
+      "ハン",
+      "Felix",
+      "フィリックス",
+      "Hyunjin",
+      "ヒョンジン",
+      "Lee Know",
+      "リノ",
+      "Seungmin",
+      "スンミン",
+      "I.N",
+      "アイエン"
+    ],
+  },
+
+    {
+    title: "RUN IT",
+    album: "RUN IT",
+    type: "Original Song",
+    date: "2026.06.24",
+    url: "song/RUN IT.html",
+    image: "images/RUN IT.jpg",
+
+    members: [
+      "Bang Chan",
+      "バンチャン",
+      "Changbin",
+      "チャンビン",
+      "HAN",
+      "ハン",
+      "Felix",
+      "フィリックス",
+      "Hyunjin",
+      "ヒョンジン",
+      "Lee Know",
+      "リノ",
+      "Seungmin",
+      "スンミン",
+      "I.N",
+      "アイエン"
+    ],
+  },
+
+      {
+    title: "STAY",
+    album: "STAY",
+    type: "Original Song",
+    date: "2026.07.10",
+    url: "song/STAY.html",
+    image: "images/STAY.jpg",
+
+    members: [
+      "Bang Chan",
+      "バンチャン",
+      "Changbin",
+      "チャンビン",
+      "HAN",
+      "ハン",
+      "Felix",
+      "フィリックス",
+      "Hyunjin",
+      "ヒョンジン",
+      "Lee Know",
+      "リノ",
+      "Seungmin",
+      "スンミン",
+      "I.N",
+      "アイエン"
+    ],
+  },
+
+        {
+    title: "Endless Sun",
+    album: "Endless Sun",
+    type: "Original Song",
+    date: "2026.07.10",
+    url: "song/Endless Sun.html",
+    image: "images/Endless Sun.jpg",
+
+    members: [
+      "Bang Chan",
+      "バンチャン",
+      "Changbin",
+      "チャンビン",
+      "HAN",
+      "ハン",
+      "Felix",
+      "フィリックス",
+      "Hyunjin",
+      "ヒョンジン",
+      "Lee Know",
+      "リノ",
+      "Seungmin",
+      "スンミン",
+      "I.N",
+      "アイエン"
+    ],
+  },
+
+  {
+    title: "Do It",
+    album: "Do It",
+    type: "Original Song",
+    date: "2026.07.10",
+    url: "song/Do It.html",
+    image: "images/Do It.jpg",
+
+    members: [
+      "Bang Chan",
+      "バンチャン",
+      "Changbin",
+      "チャンビン",
+      "HAN",
+      "ハン",
+      "Felix",
+      "フィリックス",
+      "Hyunjin",
+      "ヒョンジン",
+      "Lee Know",
+      "リノ",
+      "Seungmin",
+      "スンミン",
+      "I.N",
+      "アイエン"
+    ],
+  },
+
+    {
+    title: "I Like It",
+    album: "ATE",
+    type: "Original Song",
+    date: "2026.06.29",
+    url: "song/I Like It.html",
+    image: "images/I Like It.jpg",
+
+    members: [
+      "Bang Chan",
+      "バンチャン",
+      "Changbin",
+      "チャンビン",
+      "HAN",
+      "ハン",
+      "Felix",
+      "フィリックス",
+      "Hyunjin",
+      "ヒョンジン",
+      "Lee Know",
+      "リノ",
+      "Seungmin",
+      "スンミン",
+      "I.N",
+      "アイエン"
+    ],
+  },
+
+  {
+    title: "JJAM",
+    album: "ATE",
+    type: "Original Song",
+    date: "2026.07.26",
+    url: "song/JJAM.html",
+    image: "images/JJAM.jpg",
+
+    members: [
+      "Bang Chan",
+      "バンチャン",
+      "Changbin",
+      "チャンビン",
+      "HAN",
+      "ハン",
+      "Felix",
+      "フィリックス",
+      "Hyunjin",
+      "ヒョンジン",
+      "Lee Know",
+      "リノ",
+      "Seungmin",
+      "スンミン",
+      "I.N",
+      "アイエン"
+    ],
+  },
+
+
+
+
+
+      {
+    title: "back to life",
+    album: "back to life",
+    type: "Original Song",
+    date: "2026.06.29",
+    url: "song/back to life.html",
+    image: "images/back to life.jpg",
+
+    members: [
+      "HAN",
+      "ハン",
+    ],
+  },
+
+
+
+  {
+    title: "CASE 143",
+    album: "MAXIDENT",
+    type: "Original Song",
+    date: "2026.06.12",
+    url: "song/CASE143.html",
+    image: "images/CASE143.jpg",
+
+    members: [
+      "Bang Chan",
+      "Lee Know",
+      "Changbin",
+      "Hyunjin",
+      "HAN",
+      "Felix",
+      "Seungmin",
+      "I.N"
+    ],
+
+    lyrics: `
+      CASE 143
+      love
+      heart
+      why do I keep getting attracted
+    `
+  }
+];
